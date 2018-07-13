3c70213b8327b896a04e4a31601a8b681361ec94
Jelle Druyts <jelle_druyts@yahoo.com>
2018-07-13 09:29:16 +0200

    Removed friendly URLs and mobile views to simplify and better reflect legacy app

--- identity-applications/src/modernize-intranet-webapp/Contoso.Intranet.WebApp/Global.asax.cs
+++ identity-applications/src/modernize-intranet-webapp/Contoso.Intranet.WebApp/Global.asax.cs
@@ -14,7 +14,6 @@ namespace Contoso.Intranet.WebApp
         void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
